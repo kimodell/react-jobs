b99f4cf2437da69a6b99c5388543e45e4e43ae04
Kim O'Dell <kimbo.odell@gmail.com>
2025-11-23 12:45:09 -0500

    Add jsx to render edit job page, initialize state to current job data, add submit form logic

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,19 @@ const App = () => {
     return;
   };
 
+  //Edit job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
@@ -42,7 +55,7 @@ const App = () => {
         <Route path="add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route
           path="/edit-job/:id"
-          element={<EditJobPage />}
+          element={<EditJobPage updateJobSubmit={updateJob} />}
           loader={jobLoader}
         />
         <Route
