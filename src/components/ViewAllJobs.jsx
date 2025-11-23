1beae58f32a9f6cc930dc44299b1c9e2b83ecd9c
Kim O'Dell <kimbo.odell@gmail.com>
2025-11-23 12:55:36 -0500

    Correct Link classname

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -5,7 +5,7 @@ const ViewAllJobs = () => {
     <section className="m-auto max-w-lg my-10 px-6">
       <Link
         to="/jobs"
-        classNames="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
       >
         View All Jobs
       </Link>
